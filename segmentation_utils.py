bd364b7ba01caf675bb7064081c44f3d44d4009a
julenbhy <julenbhy@gmail.com>
2021-08-05 15:06:42 +0200

    more utils

--- segmentation_utils.py
+++ segmentation_utils.py
@@ -42,7 +42,7 @@ def get_masks(path, img_size=512):
     returns a list containing all the masks generated from the .png images of the specified directory
     :path: the path to de directory
     :img_size: the size in which the masks will be loaded (default=512)
-    :return: the list of hot encoded masks (in num_classes channels)
+    :return: the list of hot encoded(categorical) masks (in num_classes channels)
     :return: the number of detected classes
     """
     #Capture mask/label info as a list
@@ -58,16 +58,16 @@ def get_masks(path, img_size=512):
     #Convert list to array for machine learning processing          
     train_masks = np.array(train_masks)
     
-    #Encode labels... but multi dim array so need to flatten, encode and reshape
+    #Encode labels in case image is codified in colors
     labelencoder = LabelEncoder()
     n, h, w = train_masks.shape
     train_masks_reshaped = train_masks.reshape(-1,1)
     #transform colours into labels
     train_masks_reshaped_encoded = labelencoder.fit_transform(train_masks_reshaped)
     train_masks_encoded= train_masks_reshaped_encoded.reshape(n, h, w)
+    
     #detect number of classes in the masks
     num_classes = len(np.unique(train_masks_encoded))
-    #train_masks_encoded = np.expand_dims(train_masks_encoded, axis=3) #only if grayscale
     train_masks_cat = tf.keras.utils.to_categorical(train_masks_encoded, num_classes=num_classes)
     return(train_masks_cat, num_classes)
 
@@ -104,6 +104,21 @@ def get_class_weights(path, img_size=512):
     return (class_weights)
 
 
+def plot_legend(classes, cmap='viridis', size=2):
+    
+    x = []
+    my_xticks = []
+    for i in range(len(classes)):
+        x.append(i)
+        my_xticks.append(classes[i])
+        
+    f = plt.figure(figsize = (size, size))
+    f.add_subplot(1,1,1)
+    plt.yticks(x, my_xticks)
+    plt.xticks([], [])
+    x = np.reshape(x,(1,len(classes))).T
+    plt.imshow(x, cmap=cmap)
+
 def plot_mask(images, masks, num_plots=1, cmap='viridis', size=10):
     """
     plots images and masks from lists using matplotlib.pyplot
@@ -114,14 +129,15 @@ def plot_mask(images, masks, num_plots=1, cmap='viridis', size=10):
     """
     for i in range(num_plots):
         f = plt.figure(figsize = (size, size))
-        f.add_subplot(1,2, 1)
+        f.add_subplot(1,3, 1)
         plt.axis('off')
         plt. title('Original image')
         plt.imshow(images[i])
-        f.add_subplot(1,2,2)
+        f.add_subplot(1,3,2)
         plt.axis('off')
         plt. title('Ground truth mask')
         plt.imshow(masks[i], cmap=cmap)
+        
     plt.show(block=True)
     plt.show
     
@@ -160,20 +176,180 @@ def plot_prediction(images, masks, predictions, num_plots=1, cmap='viridis', siz
     
     
     
+
+def get_generator_from_directory(path, num_classes, mode, augmentation=True, val_split=0.2, 
+                                 img_size=256, batch_size=32, seed=123, backbone_preprocess=None):
+    """
+    Returns a generator for both input images and masks(hot encoded).
+    dataset must be structured in "images" and "masks" directories
+    :param path: path to the target dir containing images and masks directories
+    :num_classes: the number of classes
+    :mode: spicify whether is training or validation split
+    :augmentation: boolean for performing data augmentation (default=True)
+    :val_split: the validation split (default=0.2)
+    :img_size:
+    :batch_size:
+    :seed:
+    :backbone_preprocess:
+    :return: generator
+    """ 
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    
+    if(augmentation):
+        data_gen_args = dict(validation_split=val_split,
+                             horizontal_flip=True,
+                             vertical_flip=True,
+                             #rotation_range=45.,
+                             #width_shift_range=0.1,
+                             #height_shift_range=0.1,
+                             #zoom_range=0.2,
+                             #shear_range=0.05,
+                             #brightness_range=None,
+                             fill_mode='reflect', #'constant','nearest','reflect','wrap'
+                             #dtype=None,
+                             #featurewise_center=True,
+                             #featurewise_std_normalization=True,
+                             rescale=1. / 255,  #input normalizatoin
+                            ) # Best way: DigitalSreeni 216 min 19
+   
+    else: data_gen_args = dict(validation_split=val_split,
+                               rescale=1. / 255
+                              )
+
+    # same arguments in order to transform images and masks equaly
+    image_datagen = ImageDataGenerator(**data_gen_args)    
+    
+    image_generator = image_datagen.flow_from_directory(directory=path+'/images',
+                                                        #shuffle=True,
+                                                        subset=mode,  # train or validation
+                                                        class_mode=None,
+                                                        seed=seed,
+                                                        target_size=(img_size, img_size),
+                                                        batch_size=batch_size)
+
+    mask_generator = image_datagen.flow_from_directory(directory=path+'/masks',
+                                                       color_mode='grayscale',
+                                                       #shuffle=True,
+                                                       subset=mode,  # train or validation
+                                                       class_mode=None,
+                                                       seed=seed,
+                                                       target_size=(img_size, img_size),
+                                                       batch_size=batch_size)
+                                                    
+    def preprocess_data(img, mask):
+        if(backbone_preprocess): img = backbone_preprocess(img)
+        #Encode labels from 0 to NUM_CLASSES
+        labelencoder = LabelEncoder()
+        #Must be transformed to 1 dim array
+        n, h, w, c = mask.shape
+        mask = mask.reshape(-1,1)
+        #transform colours into labels
+        mask = labelencoder.fit_transform(mask)
+        mask = mask.reshape(n, h, w)
+        #Transform 1 channel label to hot encoded
+        mask = tf.keras.utils.to_categorical(mask, num_classes)
+        return (img, mask)
+    
+    generator = zip(image_generator, mask_generator)
+    for (img, mask) in generator:
+        img, mask = preprocess_data(img, mask)
+        yield (img, mask)
+        
+        
+        
+    
+def get_generator_from_list(images, masks, num_classes, mode, augmentation=True, val_split=0.2, 
+                                 img_size=256, batch_size=32, seed=123):
+    
+    """
+    Returns a generator for both input images and masks(hot encoded).
+    masks from msks list must be hot encoded(categorical)
+    :images: list containing the images
+    :masks: list containing the masks
+    :num_classes: the number of classes
+    :mode: spicify whether is training or validation split
+    :augmentation: boolean for performing data augmentation (default=True)
+    :val_split: the validation split (default=0.2)
+    :img_size:
+    :batch_size:
+    :seed:
+    :return: generator
+    """ 
+    from keras.preprocessing.image import ImageDataGenerator
+    
+    img_data_gen_args = dict(validation_split=val_split,
+                             horizontal_flip=True,
+                             vertical_flip=True,
+                             #rotation_range=45,
+                             #width_shift_range=0.3,
+                             #height_shift_range=0.3,
+                             #zoom_range=0.3,
+                             #shear_range=0.5,
+                             #brightness_range=None,
+                             fill_mode='constant', #'constant','nearest','reflect','wrap'
+                             )
+    
+    mask_data_gen_args = dict(validation_split=val_split,
+                             horizontal_flip=True,
+                             vertical_flip=True,
+                             #rotation_range=45,
+                             #width_shift_range=0.3,
+                             #height_shift_range=0.3,
+                             #zoom_range=0.3,
+                             #shear_range=0.5,
+                             #brightness_range=None,
+                             fill_mode='constant', #'constant','nearest','reflect','wrap'
+                             )
+
+    image_data_generator = ImageDataGenerator(**img_data_gen_args)
+    image_data_generator.fit(images, augment=True, seed=seed)
+    image_generator = image_data_generator.flow(images, seed=seed)
+    
+    mask_data_generator = ImageDataGenerator(**mask_data_gen_args)
+    mask_data_generator.fit(masks, augment=True, seed=seed)
+    mask_generator = mask_data_generator.flow(masks, seed=seed)
+    
+    generator = zip(image_generator, mask_generator)
+    for (img, mask) in generator:
+        yield (img, mask)
+
+    
 if __name__ == "__main__":
     
+    CLASSES = {0 : 'background',
+           5 : 'Mucosa',
+           4 : 'Linfocitos',
+           1 : 'Submucosa',
+           3 : 'Muscular',
+           2 : 'Subserosa',
+          }
+    
+    plot_legend(CLASSES)
+    
+    # get lists
     images = get_images('database/images/img')
     masks, num_classes = get_masks('database/masks/img')
- 
-    #check data
-    one_channel_masks = np.argmax(masks, axis=3) #from hot encoded to 1 channel
-    one_channel_predictions = one_channel_masks
-    plot_mask(images, one_channel_masks, num_plots=2)
-    plot_prediction(images, one_channel_masks, one_channel_predictions, num_plots=1)
-
-    print('Detected', num_classes, 'classes')
-    print(get_class_weights('database/masks/img'))
-
     
+    # get generator from direcotry
+#    generator=get_generator_from_directory('database', num_classes=6, mode='training')
+
+    # get generator from lists
+    generator=get_generator_from_list(images, masks, num_classes=6, mode='training')
+    
+    generator_images, generator_masks = generator.__next__()
+    a_mask=generator_masks[0]
+    #check data
+    one_channel_masks = np.argmax(generator_masks, axis=3) #from hot encoded to 1 channel
+    one_channel_predictions = one_channel_masks
+    
+    plot_mask(generator_images, one_channel_masks, num_plots=3)
+    plot_prediction(generator_images, one_channel_masks, one_channel_predictions, num_plots=3)
+
+    #print('\nDetected', num_classes, 'classes')
+    print(get_class_weights('database/masks/img'))
+    
+    
+
+
 
  
